4904c274dc4bcf91d71dba72ca7719d167ba0e09
Beko98 <99834191+Beko98@users.noreply.github.com>
2023-03-08 02:37:35 +0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,78 @@
 import React from "react";
+import pic_9 from "../images/pic_9.jpg";
+import pic_10 from "../images/pic_10.jpg";
+import pic_11 from "../images/pic_11.jpg";
+import pic_12 from "../images/pic_12.jpg";
+import pic_13 from "../images/pic_13.jpg";
+import pic_14 from "../images/pic_14.jpg";
+
+import vino from "../images/vino.png";
+import { TbSend } from "react-icons/tb";
+import { GoLocation } from "react-icons/go";
+import { RxEnvelopeClosed } from "react-icons/rx";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { FiInstagram } from "react-icons/fi";
+import { ImTwitter } from "react-icons/im";
+import { ImFacebook } from "react-icons/im";
+import { FaPinterestP } from "react-icons/fa";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer_container">
+      <div className="footer_divs">
+        <img className="logo" src={vino} alt="wg" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit scelerisque.
+        </p>
+        <p>
+          Quisque feugiat mattis ornare. Suspendisse sit amet dictum felis.
+          Nullam ac est id lacus, sed non mauris.
+        </p>
+      </div>
+      <div className="footer_divs">
+        <div className="send">
+          <TbSend />
+          <h3>1-677-124-44227</h3>
+        </div>
+
+        <div className="location">
+          <GoLocation />
+          <h3>184 Main Collins Street Victoria 807</h3>
+        </div>
+
+        <div className="envelope">
+          <RxEnvelopeClosed />
+          <h3>vino@info.com</h3>
+        </div>
+
+        <div className="global">
+          <AiOutlineGlobal />
+          <h3>vino.example.com</h3>
+        </div>
+
+        <div className="socials">
+          <FiInstagram className="icons" />
+          <ImTwitter className="icons" />
+          <ImFacebook className="icons" />
+          <FaPinterestP className="icons" />
+        </div>
+      </div>
+      <div className="footer_divs">
+        <h6>January 20, 2017</h6>
+        <h3>Etiam risus nunc, feugiat id commodo ut, laoreet in augue</h3>
+        <h6>December 18, 2018</h6>
+        <h3>Vestibulum scelerisque molestie enim, in scelerisque arcu</h3>
+      </div>
+      <div className="footer_divs">
+        <img src={pic_9} alt="pic_9" />
+        <img src={pic_10} alt="pic_10" />
+        <img src={pic_11} alt="pic_11" />
+        <img src={pic_12} alt="pic_12" />
+        <img src={pic_13} alt="pic_13" />
+        <img src={pic_14} alt="pic_14" />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,15 @@ import "../_reset.scss";
 import "../_styles.scss";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
-import img_6 from "../images/pic_6.png"
-import img_7 from "../images/pic_7.png"
-import img_8 from "../images/pic_8.png"
-
+import img_6 from "../images/pic_6.png";
+import img_7 from "../images/pic_7.png";
+import img_8 from "../images/pic_8.png";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <>
-    <div className="nav_container">
+      <div className="nav_container">
         <Navbar />;
       </div>
       <div className="Carousel-container">
@@ -21,105 +21,75 @@ function Home() {
       <div className="title">
         <h2>Traditional Winemaking</h2>
         <hr />
-       
       </div>
-  <div className="container">
+      <div className="container">
+        <div className="repeat">
+          <div className="left">
+            <h2>purchased farm</h2>
+            <h3>1997</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+          <div>
+            <img src={img_6} alt="pic-6" />
+          </div>
+          <div className="right">
+            <h2>first wineyard</h2>
+            <h3>1999</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+        </div>
 
-    {/* <div className="left">
-      <h2>purchased farm</h2>
-      <h3>1997</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <h2>purchased farm</h2>
-      <h3>2001</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <h2>purchased farm</h2>
-      <h3>2005</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </div>
+        <div className="repeat">
+          <div className="left">
+            <h2>first production</h2>
+            <h3>2001</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+          <div>
+            <img src={img_7} alt="pic-7" />
+          </div>
+          <div className="right">
+            <h2>first customers</h2>
+            <h3>2003</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+        </div>
 
-    <div className="center_images">
-      <img src={img_6} alt="pic-6" />
-      <img src={img_7} alt="pic-7" />
-      <img className="bottle" src={img_8} alt="pic-8" />
-    </div>
-
-    <div className="right">
-      <h2>first wineyard</h2>
-      <h3>1999</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <h2>first wineyard</h2>
-      <h3>2003</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <h2>first wineyard</h2>
-      <h3>2007</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </div> */}
-
-<div className="repeat">
-
-    <div className="left">
-    <h2>purchased farm</h2>
-    <h3>1997</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-
-    </div>
-    <div>
-    <img src={img_6} alt="pic-6" />
-    </div>
-    <div className="right">
-    <h2>first wineyard</h2>
-    <h3>1999</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-
-    </div>
-</div>
-
-
-
-<div className="repeat">
-
-    <div className="left">
-    <h2>first production</h2>
-    <h3>2001</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-
-    </div>
-    <div>
-    <img src={img_7} alt="pic-7" />
-
-    </div>
-    <div className="right">
-    <h2>first customers</h2>
-    <h3>2003</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-
-    </div>
-</div>
-
-
-
-<div className="repeat">
-
-    <div className="left">
-    <h2>small enterprise</h2>
-    <h3>2005</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-    </div>
-    <div>
-    <img className="bottle" src={img_8} alt="pic-8" />
-
-    </div>
-    <div className="right">
-    <h2>organic company</h2>
-    <h3>2007</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus quam ut lectus porttitor, ut ultricies libero cursus.</p>
-    </div>
-</div>
-
-
-
-
-  </div>
+        <div className="repeat">
+          <div className="left">
+            <h2>small enterprise</h2>
+            <h3>2005</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+          <div>
+            <img className="bottle" src={img_8} alt="pic-8" />
+          </div>
+          <div className="right">
+            <h2>organic company</h2>
+            <h3>2007</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              faucibus quam ut lectus porttitor, ut ultricies libero cursus.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
