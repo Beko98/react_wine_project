ff582d9990f51523254dc7cf6ff7bc42c1df3f9a
Beko98 <99834191+Beko98@users.noreply.github.com>
2023-03-14 23:27:16 +0400

    Update products.js

--- src/products.js
+++ src/products.js
@@ -8,60 +8,59 @@ import pic_22 from "../src/images/pic_22.jpeg";
 import pic_23 from "../src/images/pic_23.jpeg";
 import pic_24 from "../src/images/pic_24.jpeg";
 
-
-export const PRODUCTS =[
-    {
-        id: 1,
-        productName: "CHATEAU REUSEC",
-        price: 31.99,
-        productImage: pic_16,
-    },
-    {
-        id: 2,
-        productName: "CHÂTEAU PESQUIÉ",
-        price: 77.99,
-        productImage: pic_17,
-    },
-    {
-        id: 3,
-        productName: "COLTIBUONO",
-        price: 41.11,
-        productImage: pic_18,
-    },
-    {
-        id: 4,
-        productName: "BAUMARD",
-        price: 59.99,
-        productImage: pic_19,
-    },
-    {
-        id: 5,
-        productName: "BANDOL",
-        price: 89.99,
-        productImage: pic_20,
-    },
-    {
-        id: 6,
-        productName: "VIGNETI DEL FOL",
-        price: 69.99,
-        productImage: pic_21,
-    },
-    {
-        id: 7,
-        productName: "CHTEAU DEREZLA",
-        price: 121.21,
-        productImage: pic_22,
-    },
-    {
-        id: 8,
-        productName: "MONEIRE",
-        price: 44.44,
-        productImage: pic_23,
-    },
-    {
-        id: 9,
-        productName: "ZIND-HUMBRECHT",
-        price: 99.81,
-        productImage: pic_24,
-    }
-]
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "CHATEAU REUSEC",
+    price: 31.99,
+    productImage: pic_16,
+  },
+  {
+    id: 2,
+    productName: "CHÂTEAU PESQUIÉ",
+    price: 77.99,
+    productImage: pic_17,
+  },
+  {
+    id: 3,
+    productName: "COLTIBUONO",
+    price: 41.11,
+    productImage: pic_19,
+  },
+  {
+    id: 4,
+    productName: "BAUMARD",
+    price: 59.99,
+    productImage: pic_19,
+  },
+  {
+    id: 5,
+    productName: "BANDOL",
+    price: 89.99,
+    productImage: pic_20,
+  },
+  {
+    id: 6,
+    productName: "VIGNETI DEL FOL",
+    price: 69.99,
+    productImage: pic_21,
+  },
+  {
+    id: 7,
+    productName: "CHTEAU DEREZLA",
+    price: 121.21,
+    productImage: pic_22,
+  },
+  {
+    id: 8,
+    productName: "MONEIRE",
+    price: 44.44,
+    productImage: pic_23,
+  },
+  {
+    id: 9,
+    productName: "ZIND-HUMBRECHT",
+    price: 99.81,
+    productImage: pic_24,
+  },
+];
